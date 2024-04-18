10e89413dd2bbc1a16e6fbb2d091a59b3a625a89
Alex Mit <klepoz.672@mail.ru>
2024-04-18 16:12:33 +0300

    colors updated

--- app/src/main/java/com/example/mobile_bvt2002_mitrofanov/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobile_bvt2002_mitrofanov/ui/theme/Color.kt
@@ -12,13 +12,18 @@ object AppColors{
     val LightCream: Color = Color(0xFFFFF6EB)
     val Red: Color = Color(0xFFD6185D)
     val White: Color = Color(0xFFFFFFFF)
+    val White05 = White.copy(alpha = 0.05f)
+    val White20 = White.copy(alpha = 0.2f)
     val White30 = White.copy(alpha = 0.3f)
     val GrayLight: Color = Color(0xFFE5E5E5)
     val Gray = Color.Gray
     val Gray40 = Gray.copy(alpha = 0.5f)
     val GrayDark: Color = Color(0xFF656872)
+    val GrayDark50 = GrayDark.copy(alpha = 0.5f)
     val Dark: Color = Color(0xFF080E1E)
+    val Dark05 = Dark.copy(alpha = 0.05f)
     val Dark20 = Dark.copy(alpha = 0.2f)
+    val Dark30 = Dark.copy(alpha = 0.3f)
     val DarkLight: Color = Color(0xFF1B2336)
     val DarkLighter: Color = Color(0xFF363B44)
 
